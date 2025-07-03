58006a78ab1aad1696838094a44194065d38c535
janette-jerusal <janette.jerusal2@rtx.com>
2025-07-03 11:30:28 -0400

    Create ThisAddIn.cs

---
+++ ThisAddIn.cs
@@ -0,0 +1,9 @@
+using System;
+namespace UserStorySimilarityAddIn
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, EventArgs e) { }
+        private void ThisAddIn_Shutdown(object sender, EventArgs e) { }
+    }
+}
