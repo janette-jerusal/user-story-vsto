d1b85e084f9d7a33b741d3db9b1b5c877f328d39
janette-jerusal <janette.jerusal2@rtx.com>
2025-07-07 23:16:45 -0400

    Update ExcelWriter.cs

--- ExcelWriter.cs
+++ ExcelWriter.cs
@@ -1,24 +1,14 @@
+// ExcelWriter.cs
 using System.Collections.Generic;
-using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
 
-namespace UserStorySimilarityAddIn
+public static class ExcelWriter
 {
-    public static class ExcelWriter
+    public static void WriteSimilarities(Excel.Worksheet worksheet, List<string> results)
     {
-        public static void WriteResultsToNewSheet(List<(string idA, string idB, double score)> matches)
+        for (int i = 0; i < results.Count; i++)
         {
-            var app = Globals.ThisAddIn.Application;
-            var sheet = app.Worksheets.Add();
-            sheet.Name = "Similarity Results";
-            sheet.Cells[1,1] = "Story A ID";
-            sheet.Cells[1,2] = "Story B ID";
-            sheet.Cells[1,3] = "Similarity Score";
-            for (int i = 0; i < matches.Count; i++)
-            {
-                sheet.Cells[i+2, 1] = matches[i].idA;
-                sheet.Cells[i+2, 2] = matches[i].idB;
-                sheet.Cells[i+2, 3] = matches[i].score;
-            }
+            worksheet.Cells[i + 1, 2].Value2 = results[i];
         }
     }
 }
