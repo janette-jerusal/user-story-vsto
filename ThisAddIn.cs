865175d3d3b8033e18280b6fcbba40ce77b0dd55
janette-jerusal <janette.jerusal2@rtx.com>
2025-07-07 22:21:36 -0400

    Update ThisAddIn.cs

--- ThisAddIn.cs
+++ ThisAddIn.cs
@@ -1,16 +1,16 @@
 using System;
+using Microsoft.Office.Core;
 
 namespace UserStorySimilarityAddIn
 {
     public partial class ThisAddIn
     {
         private void ThisAddIn_Startup(object sender, EventArgs e) { }
-
         private void ThisAddIn_Shutdown(object sender, EventArgs e) { }
 
-        protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+        protected override IRibbonExtensibility CreateRibbonExtensibilityObject()
         {
-            return new Ribbon1();
+            return new Ribbon1() as IRibbonExtensibility;
         }
     }
 }
