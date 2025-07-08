a136cd686c7b8a129a0dbd955cdba8c5006d03c0
janette-jerusal <janette.jerusal2@rtx.com>
2025-07-08 00:29:58 -0400

    Update Ribbon1.cs

--- Ribbon1.cs
+++ Ribbon1.cs
@@ -1,22 +1,53 @@
-// Ribbon1.cs
+using System;
+using System.Data;
+using System.Windows.Forms;
 using Microsoft.Office.Tools.Ribbon;
-using Excel = Microsoft.Office.Interop.Excel;
 
-public partial class Ribbon1 : RibbonBase
+namespace UserStorySimilarityAddIn
 {
-    public Ribbon1() : base(Globals.Factory.GetRibbonFactory())
+    public partial class Ribbon1
     {
-        InitializeComponent();
-    }
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+        }
 
-    private void Ribbon1_Load(object sender, RibbonUIEventArgs e) { }
+        private void CompareButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            try
+            {
+                var app = Globals.ThisAddIn.Application;
+                var workbook = app.ActiveWorkbook;
 
-    private void CompareButton_Click(object sender, RibbonControlEventArgs e)
-    {
-        Excel.Worksheet sheet = Globals.ThisAddIn.Application.ActiveSheet;
-        var stories = ExcelReader.ReadUserStories(sheet);
-        var results = UserStoryComparer.Compare(stories);
-        ExcelWriter.WriteSimilarities(sheet, results);
+                if (workbook == null)
+                {
+                    MessageBox.Show("No workbook is open.");
+                    return;
+                }
+
+                var sheets = workbook.Sheets;
+                if (sheets.Count < 2)
+                {
+                    MessageBox.Show("Please ensure at least two sheets are present.");
+                    return;
+                }
+
+                var df1 = ExcelReader.ReadSheetToDataTable(sheets[1]);
+                var df2 = ExcelReader.ReadSheetToDataTable(sheets[2]);
+
+                var result = UserStoryComparer.CompareUserStories(df1, df2, 0.5); // 0.5 = similarity threshold
+
+                var newSheet = workbook.Sheets.Add();
+                newSheet.Name = "ComparisonResult";
+
+                ExcelWriter.WriteDataTableToSheet(result, newSheet);
+
+                MessageBox.Show("Comparison complete!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
     }
 }
 
