2661c15785325c408e366070694c4bbc8cbd70b8
janette-jerusal <janette.jerusal2@rtx.com>
2025-07-08 00:57:35 -0400

    Update ThisAddIn.cs

--- ThisAddIn.cs
+++ ThisAddIn.cs
@@ -1,28 +1,40 @@
 using System;
-using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using Microsoft.Office.Tools.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
 
 namespace UserStorySimilarityAddIn
 {
     public partial class ThisAddIn
     {
-        private void ThisAddIn_Startup(object sender, EventArgs e)
+        protected override Office.IRibbonExtensibility CreateRibbonExtensibilityObject()
         {
-            MessageBox.Show("UserStorySimilarityAddIn Loaded!");
+            return new Ribbon1(Globals.Factory.GetRibbonFactory());
         }
 
-        private void ThisAddIn_Shutdown(object sender, EventArgs e)
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
-            // Cleanup if needed
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
         }
 
         #region VSTO generated code
 
         private void InternalStartup()
         {
-            this.Startup += new EventHandler(ThisAddIn_Startup);
-            this.Shutdown += new EventHandler(ThisAddIn_Shutdown);
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
         }
+
+        #endregion
+    }
+}
+
 
         #endregion
     }
