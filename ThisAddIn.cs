3e32fb431317601bbec799738e9ad4ebc8a7a6b8
janette-jerusal <janette.jerusal2@rtx.com>
2025-07-07 22:07:00 -0400

    Update ThisAddIn.cs

--- ThisAddIn.cs
+++ ThisAddIn.cs
@@ -1,9 +1,16 @@
 using System;
+
 namespace UserStorySimilarityAddIn
 {
     public partial class ThisAddIn
     {
         private void ThisAddIn_Startup(object sender, EventArgs e) { }
+
         private void ThisAddIn_Shutdown(object sender, EventArgs e) { }
+
+        protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+        {
+            return new Ribbon1();
+        }
     }
 }
