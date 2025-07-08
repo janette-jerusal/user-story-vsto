017008c8dc5fb026b6bbc870f580eb0e50f22f70
janette-jerusal <janette.jerusal2@rtx.com>
2025-07-07 23:57:00 -0400

    Update ThisAddIn.cs

--- ThisAddIn.cs
+++ ThisAddIn.cs
@@ -1,8 +1,26 @@
-// ThisAddIn.cs
-using Microsoft.Office.Tools.Ribbon;
+using System;
+using Microsoft.Office.Tools;
 
-public partial class ThisAddIn
+namespace UserStorySimilarityAddIn
 {
-    private void ThisAddIn_Startup(object sender, System.EventArgs e) { }
-    private void ThisAddIn_Shutdown(object sender, System.EventArgs e) { }
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, EventArgs e)
+        {
+        }
+
+        private void ThisAddIn_Shutdown(object sender, EventArgs e)
+        {
+        }
+
+        #region VSTO generated code
+
+        private void InternalStartup()
+        {
+            this.Startup += new EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new EventHandler(ThisAddIn_Shutdown);
+        }
+
+        #endregion
+    }
 }
