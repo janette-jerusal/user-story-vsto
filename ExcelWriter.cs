72d89c1c071a75faa64d3070aa4f2c0e86dbfbb4
janette-jerusal <janette.jerusal2@rtx.com>
2025-07-08 00:34:46 -0400

    Update ExcelWriter.cs

--- ExcelWriter.cs
+++ ExcelWriter.cs
@@ -1,14 +1,19 @@
-// ExcelWriter.cs
-using System.Collections.Generic;
+using System.Data;
 using Excel = Microsoft.Office.Interop.Excel;
 
 public static class ExcelWriter
 {
-    public static void WriteSimilarities(Excel.Worksheet worksheet, List<string> results)
+    public static void WriteDataTableToSheet(DataTable dt, object sheetObj)
     {
-        for (int i = 0; i < results.Count; i++)
-        {
-            worksheet.Cells[i + 1, 2].Value2 = results[i];
-        }
+        var sheet = (Excel.Worksheet)sheetObj;
+
+        // Headers
+        for (int c = 0; c < dt.Columns.Count; c++)
+            sheet.Cells[1, c + 1] = dt.Columns[c].ColumnName;
+
+        // Data
+        for (int r = 0; r < dt.Rows.Count; r++)
+            for (int c = 0; c < dt.Columns.Count; c++)
+                sheet.Cells[r + 2, c + 1] = dt.Rows[r][c];
     }
 }
