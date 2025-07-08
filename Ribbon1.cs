d3c9615f952b4084ae41fe17d32c9e1bbbec54ce
janette-jerusal <janette.jerusal2@rtx.com>
2025-07-08 00:41:36 -0400

    Update Ribbon1.cs

--- Ribbon1.cs
+++ Ribbon1.cs
@@ -1,46 +1,35 @@
+using Microsoft.Office.Tools.Ribbon;
 using System;
 using System.Data;
 using System.Windows.Forms;
-using Microsoft.Office.Tools.Ribbon;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace UserStorySimilarityAddIn
 {
     public partial class Ribbon1
     {
-        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
-        {
-        }
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e) { }
 
-        private void CompareButton_Click(object sender, RibbonControlEventArgs e)
+        private void compareButton_Click(object sender, RibbonControlEventArgs e)
         {
             try
             {
                 var app = Globals.ThisAddIn.Application;
                 var workbook = app.ActiveWorkbook;
-
-                if (workbook == null)
-                {
-                    MessageBox.Show("No workbook is open.");
-                    return;
-                }
-
                 var sheets = workbook.Sheets;
+
                 if (sheets.Count < 2)
                 {
-                    MessageBox.Show("Please ensure at least two sheets are present.");
+                    MessageBox.Show("Workbook must have at least 2 sheets.");
                     return;
                 }
 
                 var df1 = ExcelReader.ReadSheetToDataTable(sheets[1]);
                 var df2 = ExcelReader.ReadSheetToDataTable(sheets[2]);
 
-                var result = UserStoryComparer.CompareUserStories(df1, df2, 0.5); // 0.5 = similarity threshold
+                var result = UserStoryComparer.CompareUserStories(df1, df2, 0.5);
 
-                var newSheet = workbook.Sheets.Add();
-                newSheet.Name = "ComparisonResult";
-
-                ExcelWriter.WriteDataTableToSheet(result, newSheet);
-
+                ExcelWriter.WriteDataTableToSheet(result, sheets.Add());
                 MessageBox.Show("Comparison complete!");
             }
             catch (Exception ex)
