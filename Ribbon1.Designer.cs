30ff44220076d855832b98a92b9b8be3f65ec738
janette-jerusal <janette.jerusal2@rtx.com>
2025-07-08 00:40:31 -0400

    Update Ribbon1.Designer.cs

--- Ribbon1.Designer.cs
+++ Ribbon1.Designer.cs
@@ -2,15 +2,9 @@ namespace UserStorySimilarityAddIn
 {
     partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
     {
-        private RibbonTab tab1;
-        private RibbonGroup group1;
-        internal RibbonButton compareButton;
-
-        public Ribbon1()
-            : base(Globals.Factory.GetRibbonFactory())
-        {
-            InitializeComponent();
-        }
+        private Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        private Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        private Microsoft.Office.Tools.Ribbon.RibbonButton compareButton;
 
         private void InitializeComponent()
         {
@@ -18,50 +12,20 @@ namespace UserStorySimilarityAddIn
             this.group1 = this.Factory.CreateRibbonGroup();
             this.compareButton = this.Factory.CreateRibbonButton();
 
-            // 
-            // tab1
-            // 
-            this.tab1.Label = "UserStory Tools";
-            this.tab1.Name = "tab1";
+            // Tab
+            this.tab1.Label = "User Story Tools";
             this.tab1.Groups.Add(this.group1);
 
-            // 
-            // group1
-            // 
-            this.group1.Label = "Actions";
-            this.group1.Name = "group1";
+            // Group
+            this.group1.Label = "Similarity";
             this.group1.Items.Add(this.compareButton);
 
-            // 
-            // compareButton
-            // 
-            this.compareButton.Label = "Compare";
-            this.compareButton.Name = "compareButton";
-            this.compareButton.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.CompareButton_Click);
+            // Button
+            this.compareButton.Label = "Compare User Stories";
+            this.compareButton.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.compareButton_Click);
 
-            // 
-            // Ribbon1
-            // 
-            this.Name = "Ribbon1";
-            this.RibbonType = "Microsoft.Excel.Workbook";
             this.Tabs.Add(this.tab1);
-            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
-        }
-
-        #region Component Designer generated code
-
-        /// <summary> 
-        /// Required method for Designer support - do not modify 
-        /// the contents of this method with the code editor.
-        /// </summary>
-        #endregion
-    }
-
-    partial class ThisRibbonCollection
-    {
-        internal Ribbon1 Ribbon1
-        {
-            get { return this.GetRibbon<Ribbon1>(); }
         }
     }
 }
+
